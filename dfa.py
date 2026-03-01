eebd0229247841844a732cf16d5334e4e9c3f32f
Stefan Ghetu <106436265+ursus161@users.noreply.github.com>
2026-03-01 11:45:19 +0200

    refactor parse func

--- dfa.py
+++ dfa.py
@@ -1,20 +1,5 @@
-import pprint 
-
-def parse_input(filename):
-    with open(filename,'r') as f:
-        lines = f.readlines()
-
-        states= lines[0].replace('\n','')
-        alphabet= lines[1].replace('\n','').split(',')
-        final_states= lines[2].replace('\n','').split(';')
-        initial_state=lines[3].replace('\n','')
-
-        transition_function = [el.strip('\n').split(':') for el in lines[4:]]
-        transition_function = [(el[0],el[1].split(';')) for el in transition_function]
-        transition_function= dict([(el[0], dict([element.split(',') for element in el[1]])) for el in transition_function])
-
-
-        return ((states, alphabet, transition_function, initial_state, final_states))
+import pprint
+from parse import parse_input
 
 
 states, alphabet, transition_function, initial_state, final_states = parse_input('input.txt')
@@ -51,4 +36,4 @@ def dfa_acceptor(dfa, input_string):
     return current_state in final_states
  
 print(dfa_acceptor(parse_input('input.txt'), input('Introdu un cuvant pentru a il verifica daca este acceptat sau nu: ')))
-
+ 
---
+++ parse.py
@@ -0,0 +1,15 @@
+def parse_input(filename):
+    with open(filename,'r') as f:
+        lines = f.readlines()
+
+        states= lines[0].replace('\n','')
+        alphabet= lines[1].replace('\n','').split(',')
+        final_states= lines[2].replace('\n','').split(';')
+        initial_state=lines[3].replace('\n','')
+
+        transition_function = [el.strip('\n').split(':') for el in lines[4:]]
+        transition_function = [(el[0],el[1].split(';')) for el in transition_function]
+        transition_function= dict([(el[0], dict([element.split(',') for element in el[1]])) for el in transition_function])
+
+
+        return ((states, alphabet, transition_function, initial_state, final_states))
